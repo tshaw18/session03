cc69df6b45d82b114ea4d26e51a3c38e859bc57b
Trevor Shaw <tshaw18@my.whitworth.edu>
2015-05-28 20:47:35 -0500

    Create student.cpp

---
+++ student.cpp
@@ -0,0 +1,14 @@
+#include "student.h"
+
+void Study(){
+}
+void DoHomework(){
+    
+}
+bool Register(string className){
+    return true;
+}
+bool ChangeAdvisor(string name){
+    return true;
+}
+
