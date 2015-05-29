70b3fabc80491196b59d8081e6345c6f8d188cdf
Trevor Shaw <tshaw18@my.whitworth.edu>
2015-05-28 20:47:01 -0500

    Create faculty.cpp

---
+++ faculty.cpp
@@ -0,0 +1,16 @@
+#include "faculty.h"
+
+int Faculty :: GetID(){
+    return id;
+}
+void Faculty :: SetID(){
+    if (id > 0 && id < 10000)
+        NewID = id;
+}
+
+void Teach(){
+    
+}
+void Advise(){
+    
+}
