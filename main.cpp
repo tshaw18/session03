c9e077a48708e7b0669e1106c76ee9ac14ff8897
Trevor Shaw <tshaw18@my.whitworth.edu>
2015-05-28 20:46:25 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "student.h"
+#include "faculty.h"
+
+ 
+using namespace std;
+
+
+int main(){
+    Student s1("Trevor Shaw", 1540106), s2("Rebecca Fedorko", NULL, false);
+    
+      
+    
+    
+}
